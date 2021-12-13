726193aca059cad2fbf60ac43d8ef7ac3846d27c
Nikita Aleshchenko <n.aleshchenko@tinkoff.ru>
2021-12-13 07:18:17 +0300

    Small transport interface fix

--- src/main/scala/bissetii/effect/Transport.scala
+++ src/main/scala/bissetii/effect/Transport.scala
@@ -17,15 +17,15 @@ object Transport {
   type _asyncTrans[R] = AsyncTrans |= R
   type _syncTrans[R]  = SyncTrans  |= R
 
-  def sendAasync[T, R :_asyncTrans](key: String, value: T): Eff[R, Unit] =
+  def sendAsync[T, R :_asyncTrans](key: String, value: T): Eff[R, Unit] =
     Eff.send[AsyncTrans, R, Unit](Send(key, value))
 
-  def receiveAasync[T, R :_asyncTrans](key: String): Eff[R, Unit] =
-    Eff.send[AsyncTrans, R, Unit](Receive(key))
+  def receiveAsync[T, R :_asyncTrans](key: String): Eff[R, T] =
+    Eff.send[AsyncTrans, R, T](Receive(key))
 
   def sendSync[T, R :_syncTrans](key: String, value: T): Eff[R, Unit] =
     Eff.send[SyncTrans, R, Unit](Send(key, value))
 
-  def receiveSync[T, R :_syncTrans](key: String): Eff[R, Unit] =
-    Eff.send[SyncTrans, R, Unit](Receive(key))
+  def receiveSync[T, R :_syncTrans](key: String): Eff[R, T] =
+    Eff.send[SyncTrans, R, T](Receive(key))
 }
